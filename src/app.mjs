b7895a568aea4cfe0bc81f7ba74984f9f7e30ff6
unknown <talmoshel444@gmail.com>
2024-08-06 14:02:38 +0300

    changes..2

--- src/app.mjs
+++ src/app.mjs
@@ -12,7 +12,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3001",
-  "https://boxing-front.onrender.com",
+  "https://appointment-front-5jsl.onrender.com",
 ];
 
 const corsOptions = {
